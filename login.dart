f2805fbaaa2f3743fa1c8d309e881d66db2be161
ajitlenka30 <ajitlenka30@gmail.com>
2022-12-13 12:17:57 +0530

    added facebook and gmail choice

--- login.dart
+++ login.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 void main(List<String> args) {
   
   print("1. number");
+  print("2 facebook");
+  print("3 gmail");
 
   stdout.write("Enter your choice:- ");
   int choice = int.parse(stdin.readLineSync()as String);
@@ -18,6 +20,58 @@ void main(List<String> args) {
     stdout.write("\nEnter your registered name: ");
     var nam = stdin.readLineSync()as String;
     stdout.write("\nEnter your registered password: ");
+    var pass = stdin.readLineSync()as String;
+    stdout.write("\n***************************************************************************");
+    if(name == nam && password == pass)
+    {
+      print("\n\nlogin success");
+      stdout.write("\n***************************************************************************");
+    }
+    else 
+    {
+      print("\n\nplease enter vail password and name");
+      stdout.write("\n***************************************************************************");
+    }
+    break;
+
+    case 2:
+    stdout.write("***************************************************************************");
+    stdout.write("\nEnter your Facebook name: ");
+    var name = stdin.readLineSync()as String;
+    stdout.write("\nEnter your facebook password: ");
+    var password = stdin.readLineSync()as String;
+    stdout.write("\n***************************************************************************");
+    stdout.write("\n\nregister complete");
+    stdout.write("\n\n***************************************************************************");
+    stdout.write("\nEnter your facebook name: ");
+    var nam = stdin.readLineSync()as String;
+    stdout.write("\nEnter your facebook password: ");
+    var pass = stdin.readLineSync()as String;
+    stdout.write("\n***************************************************************************");
+    if(name == nam && password == pass)
+    {
+      print("\n\nlogin success");
+      stdout.write("\n***************************************************************************");
+    }
+    else 
+    {
+      print("\n\nplease enter vail facebook password and name");
+      stdout.write("\n***************************************************************************");
+    }
+    break;
+
+    case 3:
+    stdout.write("***************************************************************************");
+    stdout.write("\nEnter your gmail id: ");
+    var name = stdin.readLineSync()as String;
+    stdout.write("\nEnter your gmail password: ");
+    var password = stdin.readLineSync()as String;
+    stdout.write("\n***************************************************************************");
+    stdout.write("\n\nregister complete");
+    stdout.write("\n\n***************************************************************************");
+    stdout.write("\nEnter your gmail name: ");
+    var nam = stdin.readLineSync()as String;
+    stdout.write("\nEnter your gmail password: ");
     var pass = stdin.readLineSync()as String;
     stdout.write("\n***************************************************************************");
     if(name == nam && password == pass)
