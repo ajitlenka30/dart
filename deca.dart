87ed6f83e58533e0b7e5679233ee58020f0da341
ajitlenka30 <ajitlenka30@gmail.com>
2022-12-28 22:41:32 +0530

    added atm 2 and decagon

--- deca.dart
+++ deca.dart
@@ -7,9 +7,9 @@ class Abc {
     stdout.write("Enter base :- ");
     double base = double.parse(stdin.readLineSync() as String);
 
-    double area = 5 / 2 * pow(base, 2) * sqrt(5 + 2); // * sqrt(5);
+    double area = 5/2 * pow(base, 2) * sqrt(5 + 2)* sqrt(5);
 
-     print("area of decagon := $area");
+    print("area of decagon := $area");
   }
 }
 
