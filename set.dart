f323ca7f989ff993ff47f14c66309ffd9e28d824
ajitlenka30 <ajitlenka30@gmail.com>
2023-01-03 11:00:44 +0530

    set

---
+++ set.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  Set names = <String>{"James", "Ricky", "Devansh", "Adam","Devansh", "Adam"}; // use of set for printing special data in list
+
+  print("name = $names");
+}
