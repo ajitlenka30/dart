047a86858baa7d9e2f476b27e0a51253d09825da
ajitlenka30 <ajitlenka30@gmail.com>
2023-01-05 10:52:59 +0530

    added Constructor test

---
+++ Constructor-task.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+class Test {
+  var a, b;
+  var d = 12;
+  var k = "ajit";
+  Test() {
+    var z = 20;
+    print("Enter value of A:- ");
+    this.a = 20;
+    this.b = 30;
+    print("ans = ${a + b}");
+
+  }
+}
+
+void main(List<String> args) {
+  Test();
+}
