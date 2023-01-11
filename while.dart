7b748d806a78636a3b1f19306abd828af4e6ea74
ajitlenka30 <ajitlenka30@gmail.com>
2023-01-11 18:01:20 +0530

    while loop test

---
+++ while.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  int i = 0;
+  stdout.write(
+      "***************************************************************************");
+  stdout.write("\nRegister your name :- ");
+  String? name = stdin.readLineSync();
+  stdout.write("\nRegister your pin :- ");
+  var password = stdin.readLineSync() as String;
+  stdout.write(
+      "\n***************************************************************************");
+  stdout.write("\n\nregister complete");
+  stdout.write(
+      "\n\n***************************************************************************");
+  do {
+    stdout.write("\nEnter your ATM name for login: ");
+    String? nam = stdin.readLineSync();
+    stdout.write("\nEnter your ATM pin: ");
+    var pass = stdin.readLineSync() as String;
+    stdout.write(
+        "\n***************************************************************************");
+
+    if (name == nam && password == pass) {
+      print("\n\n\t\tlogin success");
+      stdout.write(
+          "\n***************************************************************************");
+    } else {
+      print("\n\nplease enter vail password and name");
+      stdout.write(
+          "\n***************************************************************************");
+      i++;
+    }
+  } while (i < 2);
+}
