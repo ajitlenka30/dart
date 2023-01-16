9b081bb1626903a45a770281f518ac38c24d6e5b
ajitlenka30 <ajitlenka30@gmail.com>
2023-01-16 11:12:13 +0530

    some fixes and improvements

--- rajuchai2.dart
+++ rajuchai2.dart
@@ -2,54 +2,116 @@ import 'dart:io';
 
 void main(List<String> args) {
   User obj = User();
-  // obj.admin();
-  obj.user();
+  var ans;
+  do {
+    stdout.write("1. Admin");
+    stdout.write("\n2. User");
+    stdout.write(
+        "\n***************************************************************************");
+    stdout.write("\nEnter your choice:- ");
+    int userans = int.parse(stdin.readLineSync() as String);
+    stdout.write(
+        "\n***************************************************************************");
+    switch (userans) {
+      case 1:
+        obj.admin();
+        break;
+      case 2:
+        obj.user();
+        break;
+      default:
+        stdout.write(
+            "\n***************************************************************************");
+        stdout.write("invalid choice try again");
+        stdout.write(
+            "\n***************************************************************************");
+    }
+    stdout.write("\ndo you want to continue press [1] :- ");
+    ans = int.parse(stdin.readLineSync()!);
+    stdout.write(
+        "\n***************************************************************************");
+  } while (ans == 1);
 }
 
 class Admin {
+  var userans1;
   String? product1 = "chai";
   String? product2 = "coffee";
   String? product3 = "biscuit";
   void admin() {
-    stdout.write("\n4. Add product");
-    stdout.write("\n2. Remove product");
-    stdout.write("\n3. Replace product");
+    do {
+      stdout.write(
+          "\n***************************************************************************");
+      stdout.write("\n1. Add product");
+      stdout.write("\n2. Remove product");
+      stdout.write("\n3. Replace product");
+      stdout.write(
+          "\n***************************************************************************");
 
-    List product = [product1, product2, product3];
+      List product = [product1, product2, product3];
 
-    stdout.write("\n\nkeya krna hai aap ko :- ");
-    int choice = int.parse(stdin.readLineSync() as String);
+      stdout.write("\n\nkeya krna hai aap ko :- ");
+      int choice = int.parse(stdin.readLineSync() as String);
+      stdout.write(
+          "\n***************************************************************************");
 
-    switch (choice) {
-      case 1:
-        stdout.write("\ncurrent we have these product :- $product");
-        stdout.write("\nEnter new product name :- ");
-        String? add = stdin.readLineSync();
-        product.add(add);
-        stdout.write("\nsuccessfully added new product");
-        stdout.write("\nupdated product list :- $product");
-        break;
+      switch (choice) {
+        case 1:
+          stdout.write("\ncurrent we have these product :- $product");
+          stdout.write(
+              "\n***************************************************************************");
+          stdout.write("\nEnter new product name :- ");
+          String? add = stdin.readLineSync();
+          stdout.write(
+              "\n***************************************************************************");
+          product.add(add);
+          stdout.write("\nsuccessfully added new product");
+          // stdout.write("\nupdated product list :- $product");
+          stdout.write(
+              "\n***************************************************************************");
+          break;
 
-      case 2:
-        stdout.write("\ncurrent we have these product :- $product");
-        stdout.write("\nEnter name that you want to remove :- ");
-        String? remove = stdin.readLineSync();
-        product.remove(remove);
-        stdout.write("\nsuccessfully removed product");
-        stdout.write("\nupdated product list :- $product");
-        break;
+        case 2:
+          stdout.write("\ncurrent we have these product :- $product");
+          stdout.write(
+              "\n***************************************************************************");
+          stdout.write("\nEnter name that you want to remove :- ");
+          String? remove = stdin.readLineSync();
+          stdout.write(
+              "\n***************************************************************************");
+          product.remove(remove);
+          stdout.write(
+              "\n***************************************************************************");
+          stdout.write("\nsuccessfully removed product");
+          // stdout.write("\nupdated product list :- $product");
+          stdout.write(
+              "\n***************************************************************************");
+          break;
 
-      case 3:
-        stdout.write("\ncurrent we have these product :- $product");
-        stdout.write("\nEnter product name that you want to replace :- ");
-        var replace = int.parse(stdin.readLineSync() as String);
-        stdout.write("\nEnter replace name of product:- ");
-        var element = stdin.readLineSync();
-        product[replace] = element;
-        stdout.write("\nsuccessfully replaced product");
-        stdout.write("\nupdated product list:- $product");
-        break;
-    }
+        case 3:
+          stdout.write("\ncurrent we have these product :- $product");
+          stdout.write(
+              "\n***************************************************************************");
+          stdout.write("\nEnter product name that you want to replace :- ");
+          var replace = int.parse(stdin.readLineSync() as String);
+          stdout.write(
+              "\n***************************************************************************");
+          stdout.write("\nEnter replace name of product:- ");
+          var element = stdin.readLineSync();
+          stdout.write(
+              "\n***************************************************************************");
+          product[replace] = element;
+          stdout.write("\nsuccessfully replaced product");
+          // stdout.write("\nupdated product list:- $product");
+          stdout.write(
+              "\n***************************************************************************");
+          break;
+      }
+      stdout.write("\ndo you want to continue press [1] :- ");
+      userans1 = int.parse(stdin.readLineSync()!);
+      stdout.write(
+          "\n***************************************************************************");
+    } while (userans1 == 1);
   }
 }
 
@@ -64,7 +126,6 @@ class User extends Admin {
       print("2.$product2 RS 5");
       print("3.$product3 RS 10");
       print("4.Total");
-
       stdout.write(
           "***************************************************************************");
 
@@ -78,10 +139,7 @@ class User extends Admin {
           stdout.write("\nApp nai $product1 select kiya hai");
           stdout.write("\nkitna hai order :- ");
           int order1 = int.parse(stdin.readLineSync()!);
-          stdout.write(
-              "**********************************[Bill]***********************************");
           total = order1 * 2;
-          stdout.write("\ntotal hua apka :- $total");
           stdout.write(
               "\n***************************************************************************");
           break;
@@ -92,10 +150,7 @@ class User extends Admin {
           stdout.write("\nApp nai $product2 select kiya hai");
           stdout.write("\nkitna hai order :- ");
           int order2 = int.parse(stdin.readLineSync() as String);
-          stdout.write(
-              "**********************************[Bill]***********************************");
           total = order2 * 5;
-          stdout.write("\ntotal hua apka :- $total");
           stdout.write(
               "\n***************************************************************************");
           break;
@@ -107,26 +162,28 @@ class User extends Admin {
 
           stdout.write("\nkitna hai order :- ");
           int order3 = int.parse(stdin.readLineSync() as String);
-
-          stdout.write(
-              "**********************************[Bill]***********************************");
           total = order3 * 10;
-          stdout.write("\ntotal hua apka :- $total");
           stdout.write(
               "\n***************************************************************************");
           break;
         case 4:
           stdout.write(
               "**********************************[Bill]***********************************");
-          stdout.write("\n1.Order Quantity :- $total");
+          stdout.write("\nTotal bill :- $total");
           stdout.write(
               "\n***************************************************************************");
           break;
         default:
+          stdout.write(
+              "\n***************************************************************************");
           stdout.write("please enter a valid option...");
+          stdout.write(
+              "\n***************************************************************************");
       }
       stdout.write("\ndo you want to continue press [1] :- ");
       ans3 = int.parse(stdin.readLineSync()!);
+      stdout.write(
+          "\n***************************************************************************");
     } while (ans3 == 1);
   }
 }
