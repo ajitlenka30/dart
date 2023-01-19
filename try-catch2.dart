66ccfa175405bb687e3b915e924f5e72cd43eda6
ajitlenka30 <ajitlenka30@gmail.com>
2023-01-19 13:05:32 +0530

    remove dot

--- try-catch2.dart
+++ try-catch2.dart
@@ -4,10 +4,9 @@ void main() {
   try {
     print("The character at the position 5 is ${message[5]}.");
   } on RangeError {
-    print('The valid range for the index is [0..${message.length - 1}].');
+    print('The valid range for the index is [0..${message.length - 1}]');
   } catch (e) {
     print(e);
   }
-
   print('Bye!');
 }
